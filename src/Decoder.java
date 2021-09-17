ca69cc01b1f9d572826472073d95a4cf59136d2c
coliny125 <colinyuan3333@gmail.com>
2021-09-16 17:40:42 -0700

    Created Decoder Commit #1
    Still testing; works for test file 1

---
+++ src/Decoder.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.*;
+
+public class Decoder {
+	
+	//private static double maxTableSize;
+	private static String decodedOutputFileName = "decoded.txt";
+	
+	public static String decode (String inputFile, int maxDictSize) throws IOException{
+		//maxTableSize = Math.pow(2, bit_Length);
+		
+		String[] compressedValuesStr = null;
+		List<Integer> compressedValues = new ArrayList<Integer>();
+		int dictSize = 255;
+		
+		String inputIntCodes = "";
+		BufferedReader br = null;
+		br = new BufferedReader(new FileReader(inputFile));
+		inputIntCodes = br.readLine();
+        br.close();
+        compressedValuesStr = inputIntCodes.split(" ");
+        for (String str: compressedValuesStr) {
+        	int num = Integer.parseInt(str);
+        	compressedValues.add(num);
+        }
+        
+        Map<Integer, String> dict = new HashMap<Integer, String>();
+ 		for (int i = 0; i < 255; i++)
+ 			dict.put(i, "" + (char) i);
+ 		
+ 		String inputStr = ""+ (char) (int) compressedValues.get(0);
+ 		StringBuffer decodedValues = new StringBuffer();
+ 		
+ 		String newStr = null;
+ 		for (int key : compressedValues) {
+ 			if (dict.containsKey(key))
+ 				newStr = dict.get(key);
+ 			else if (key == dictSize)
+ 				newStr = inputStr + inputStr.charAt(0);
+ 			decodedValues.append(newStr);
+ 			
+ 			if(dictSize<maxDictSize)
+ 				dict.put(dictSize++, inputStr + newStr.charAt(0));
+ 			
+ 			inputStr = newStr;
+ 		}
+        createFile(decodedValues.toString());
+ 		return(decodedValues.toString());
+	}
+	
+	private static void createFile(String decodedValues) throws IOException{
+		FileWriter writer = new FileWriter(decodedOutputFileName);
+		BufferedWriter bw = new BufferedWriter(writer);
+		
+		try {
+			bw.write(decodedValues);
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void main(String[] args) throws IOException {
+		System.out.println(decode("intcodesOutput.txt", 512));
+	}
+		
+}
--- src/Encoder.java
+++ src/Encoder.java
@@ -4,52 +4,49 @@ import java.io.*;
 public class Encoder {
     private HashMap<String, Integer> dictionary;
     BufferedReader in;
-    String output;
-    int tracker;
-    int binary;
+    int dictTracker;
 
     public Encoder (String filename) throws IOException {
         in = new BufferedReader (new FileReader(new File (filename)));
         dictionary = new HashMap<String, Integer>();
-        output = "";
-        tracker = 256;
-        binary = 0;
+        dictTracker = 256;
     }
 
     public String encode () throws IOException {
-        while (true) {
-            StringBuilder cur = new StringBuilder (in.read());
-            if (in.read() >= 0) {
-                String next = in.read();
-                if (dictionary.containsKey(cur + next)) {
-                    cur.append(next);
-                }
-                else {
-                    output += cur;
-                    dictionary.put(cur + next, tracker++);
-                    cur = next;
-                }
-            }
-            else
-                break;
+    	for (int i = 0; i < 256; i++)//load ascii table 
+            dictionary.put("" + (char)i, i);
+    	StringBuilder sb = new StringBuilder ();
+    	String cur = "";
+        while (in.ready()) {
+              String next = ""+(char)in.read();
+              String combined = cur + next;
+              if (dictionary.containsKey(combined)) {
+                    cur = combined;
+              }else {
+            	  sb.append(dictionary.get(cur) + " ");
+                  dictionary.put(combined, dictTracker++);
+                  cur = next;
+              }
         }
+        
+        if (!cur.equals("")) {
+        	sb.append(dictionary.get(cur));
+        }
+        return sb.toString();
     }
 
-    /**
-     * variable naming is very poor here but they are simple placeholders to go from char to a big binary int.
-     */
-    public void toBinary () {
-        for (int i = 0; i<output.length(); i++) {
-            char cur = output.charAt(i);
-
-            //The following three lines is not my code
-            Integer a = Character.getNumericValue(cur);
-            String b = Integer.toBinaryString(a);
-            Integer binaryInteger = Integer.valueOf(b);
-
-            //back to my code :p
-            binary = (binary * 100000000) + binaryInteger;
-        }
+    public static void main (String [] args) throws IOException {
+    	FileWriter outputFile;
+    	BufferedWriter writer = null;
+    	Encoder coder = new Encoder("lzw-file1.txt");
+    	try {
+    		outputFile = new FileWriter("intcodesOutput.txt");
+    		writer = new BufferedWriter(outputFile);
+    		writer.write(coder.encode());
+    	}finally {
+    		writer.close();
+    	}
+    	
     }
-
+    
 }
